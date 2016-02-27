f5d89fee02d3d6a001ec66bef6b6037c6b83a224
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2016-02-27 14:30:48 +0100

    add lockfile, some exception handling for better error output

--- clone.py
+++ clone.py
@@ -20,13 +20,15 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 
-import json
-import urllib.request
-import json
-import re
-from pprint import pprint
-import os
 from subprocess import call
+import fcntl
+import json
+import os
+import pprint
+import re
+import sys
+import traceback
+import urllib.request
 
 USERNAME = 'sandsmark'
 
@@ -34,7 +36,6 @@ USERNAME = 'sandsmark'
 os.chdir('/srv/http/iskrembilen/git')
 
 def download_repo(name, url, description):
-#    print('downloading new repo ', url)
     call(['git', 'clone', '--mirror', url])
     origdir = os.getcwd()
     os.chdir(origdir + '/' + name)
@@ -42,8 +43,6 @@ def download_repo(name, url, description):
     os.chdir(origdir)
 
 def update_repo(name, url, description):
-#    print('updating repo', name, url)
-
     origdir = os.getcwd()
     os.chdir(origdir + '/' + name)
 
@@ -58,7 +57,6 @@ def update_repo(name, url, description):
     os.chdir(origdir)
 
 def fetch_url(url, description):
-#    print('getting url', url)
     repo_res = re.search(r'git://github\.com/' + USERNAME + r'/([^\.]+\.git)', url)
     if not repo_res:
         print('unable to parse url:', url)
@@ -71,7 +69,16 @@ def fetch_url(url, description):
         download_repo(repo_name, url, description)
 
 
+# Running multiple instances would be really bad
+fp = open('/run/lock/github-mirror.lock', 'w')
+try:
+    fcntl.lockf(fp, fcntl.LOCK_EX | fcntl.LOCK_NB)
+except IOError:
+    print('Unable to lock lockfile, already running?')
+    sys.exit(1)
+
 url = 'https://api.github.com/users/' + USERNAME + '/repos?per_page=100'
+
 while url:
     response = urllib.request.urlopen(url)
     repos_res = response.readall().decode('utf-8')
@@ -84,9 +91,17 @@ while url:
     for repo in repos:
         if 'git_url' not in repo:
             print('could not find git_url in repo')
+            pprint.pprint(repo)
             break
 
-        fetch_url(repo['git_url'], repo['description'])
+        if not 'description' in repo:
+            repo['description'] = ''
+
+        try:
+            fetch_url(repo['git_url'], repo['description'])
+        except:
+            print('failure to fetch repo ' + repo['git_url'])
+            traceback.print_exc()
 
     # fucking github limits the number of repos on a page...
     link_header = response.getheader('Link')
